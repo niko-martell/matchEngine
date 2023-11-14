bb839a18683b2d75f5392a5479e4b9dd9a1a5873
sky-jo <99069078+sky-jo@users.noreply.github.com>
2023-11-13 20:48:53 -0700

    Update scholarship.java

--- scholarship.java
+++ scholarship.java
@@ -1,118 +1,67 @@
+/*
+ * For: SFWE 301 Final Project
+ * Purpose: This class contains the information about Students in the 
+ * 	        UASAMS. There is a string containing the name and attributes 
+ * 			containing information about the student outlined in the 
+ * 			requirements. This class acts as a container for data to be
+ * 			used by the Match Engine to generate a fit rating.
+ * Setters are not provided as this information should not change with regards
+ * to the matching engine.
+ */
+import java.util.HashMap;
+
 public class Scholarship {
-    private String name;
-    private String[] ammount;
-    private String[] fields, attributes;
-    private float gpa;
-    private String year;
-    private String openDate;
-    private String dueDate;
-
-    public Scholarship() {
-
-    }
-
-    public Scholarship(String n, String[] m, String[] f, String[] a, float g, String y, String od, String dd){
-        name = n;
-        ammount = m;
-        fields = f;
-        attributes = a;
-        gpa = g;
-        year = y;
-        openDate=od;
-        dueDate=dd;
+    private String name, openDate, dueDate, priorityAttribute;
+    private int amount; // potentially unnecessary
+    private HashMap<String, String> attributes = new HashMap<String, String>(10);
+    
+    /**
+     * @param scholarshipName the name of the scholarship
+     * @param scholarshipOpenDate open date of the scholarship in the form MM/DD/YYYY
+     * @param scholarshipDueDate due date of the scholarship in the form MM/DD/YYYY    
+     * @param scholarshipPriorityAttribute the priority attribute of the scholarship.
+     * @param scholarshipAmount the value of the scholarship in dollars.
+     * @param scholarshipAttributes info about the scholarship (indices - attribute: 0 - major, 1 - minor, 2 - graduation year, 3 - GPA, 4 - year of study, 
+     * 															5 - transfer student, 6 - units enrolled, 7 - gender, 8 - race)
+     */
+    public Scholarship(String scholarshipName,  String scholarshipOpenDate, 
+    			       String scholarshipDueDate, String scholarshipPriorityAttribute,
+    			       int scholarshipAmount, String[] scholarshipAttributes) { 
+    	name = scholarshipName;
+    	openDate = scholarshipOpenDate;
+    	dueDate = scholarshipDueDate;
+    	priorityAttribute = scholarshipPriorityAttribute;
+    	amount = scholarshipAmount;
+    	
+    	attributes.put("major", scholarshipAttributes[0]);
+    	attributes.put("minor", scholarshipAttributes[1]);
+    	attributes.put("graduation year", scholarshipAttributes[2]);
+    	attributes.put("GPA", scholarshipAttributes[3]);
+    	attributes.put("year of study", scholarshipAttributes[4]);
+    	attributes.put("transfer student", scholarshipAttributes[5]);
+    	attributes.put("units enrolled", scholarshipAttributes[6]);
+    	attributes.put("gender", scholarshipAttributes[7]);
+    	attributes.put("race", scholarshipAttributes[8]);
     }
 
     public String getName(){
         return name;
     }
-
-    public String[] getAmmount(){
-        return ammount;
-    }
-
-    public String[] getFields(){
-        return fields;
-    }
-
-    public String[] getAttributes(){
-        return attributes;
-    }
-
-    public float getGPA(){
-        return gpa;
-    }
-
-    public String getYear(){
-        return year;
-    }
+    
     public String getOpenDate(){
         return openDate;
     }
+    
     public String getDueDate(){
         return dueDate;
     }
-   
-    public boolean hasMajor(Student n){
-        String m = n.getMajor();
-        for (String maj : majors){
-            if (maj.equalsIgnoreCase(m)){
-                return true;
-            }
-        }
-        return false;
+    
+    public String getPriorityAttribute() {
+    	return priorityAttribute;
     }
-
-    public int hasFields(Student n){
-        String[] f = n.getFields();
-        int matches = 0;
-        for (String sField : f) {
-            for (String field : this.fields){
-
-                //if the matching field is found, break out of the loop and search the next field.
-                if (field.equalsIgnoreCase(sField)){
-                    matches++;
-                    break;
-                }
-            }
-        }
-        return matches;
+    
+    public HashMap<String, String> getAttributes() {
+    	return attributes;
     }
-
-    public int hasAttributes(Student n){
-        String[] a = n.getAttributes();
-        int matches = 0;
-        for (String sAtt : a){
-            for (String att : this.attributes){
-
-                //if the matching attribute is found, break out of the loop and search the next attribute.
-                if (att.equalsIgnoreCase(sAtt)){
-                    matches++;
-                }
-            }
-        }
-        return matches;
-    }
-
-    public boolean hasGPA(Student n){
-        if (n.getGPA() >= this.gpa){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-
-    //public String toString() {
-    //    return ;
-   // }
-
-   /*  public boolean hasYear(Student n){
-        if (n.getYear() >= this.year){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }*/
 
 }
